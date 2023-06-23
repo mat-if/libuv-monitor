ef6559177ae7ec8b44c32919d432ae00829a537f
Mat <mat@iflabs.network>
2023-06-23 15:49:42 -0400

    debug print build command

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ else
     TARGET_FLAG=""
 fi
 
-zig cc --print-effective-triple
-zig cc -O3 -undefined dynamic_lookup -shared -isystem $NAPI_HEADER_DIR $TARGET_FLAG -o uvc.node src/main.c
+# zig cc -print-supported-cpus
+# zig cc --print-effective-triple
+# zig cc -### -O3 -undefined dynamic_lookup -shared -isystem $NAPI_HEADER_DIR $TARGET_FLAG -o uvc.node src/main.c
+# cc -O1 -undefined dynamic_lookup -shared -isystem $NAPI_HEADER_DIR $TARGET_FLAG -o uvc.node src/main.c
+cc -O1 -fPIC -shared -isystem $NAPI_HEADER_DIR $TARGET_FLAG -o uvc.node src/main.c
 
