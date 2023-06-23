fe487df3a57128b8eb514b6c7cd6063d8a3e166d
Mat <mat@iflabs.network>
2023-06-23 15:43:07 -0400

    try building for target directly

--- build.sh
+++ build.sh
@@ -2,5 +2,13 @@ set -x
 
 NAPI_HEADER_DIR=`realpath "$(dirname $(which node))/../include/node"`
 
+if [[ -n "$1" ]]
+then
+    TARGET_FLAG="-target $1"
+else
+    TARGET_FLAG=""
+fi
+
 zig cc --print-effective-triple
-zig cc -O3 -undefined dynamic_lookup -shared -isystem $NAPI_HEADER_DIR -o uvc.node src/main.c
+zig cc -O3 -undefined dynamic_lookup -shared -isystem $NAPI_HEADER_DIR $TARGET_FLAG -o uvc.node src/main.c
+
