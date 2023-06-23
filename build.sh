d373ce2bfa72927bfee970be5024b003fad98248
Mat <mat@iflabs.network>
2023-06-23 14:10:34 -0400

    display some diag info when building

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ set -x
 
 NAPI_HEADER_DIR=`realpath "$(dirname $(which node))/../include/node"`
 
+zig cc --print-effective-triple
 zig cc -O3 -undefined dynamic_lookup -shared -isystem $NAPI_HEADER_DIR -o uvc.node src/main.c
